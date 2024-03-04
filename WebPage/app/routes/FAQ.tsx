f0ff05579048a4ca073b46e59b40ad8ce442934b
Gytis Pranauskas <gpgytis@gmail.com>
2024-03-04 23:14:12 +0300

    removed unused variables

--- WebPage/app/routes/FAQ.tsx
+++ WebPage/app/routes/FAQ.tsx
@@ -1,5 +1,5 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 
 import { getNoteListItems } from "~/models/note.server";
 import { requireUserId } from "~/session.server";
--- WebPage/app/routes/_index.tsx
+++ WebPage/app/routes/_index.tsx
@@ -1,7 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 
-import Header from "~/components/common/header/header";
 import { useOptionalUser } from "~/utils";
 
 export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
