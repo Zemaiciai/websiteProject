7981093a183840c4b1be90e499a6d2a1a4e35d08
Gytis Pranauskas <gpgytis@gmail.com>
2024-04-18 16:21:05 +0300

    removed a repetitive div, added purple background in login page

--- WebPage/app/routes/join.tsx
+++ WebPage/app/routes/join.tsx
@@ -87,97 +87,95 @@ export default function Join() {
       <div className="flex w-full h-full flex-col justify-center items-center">
         <div className="flex w-2/3 h-full my-16">
           <div className="flex relative h-full w-2/3 bg-custom-200 rounded-l-2xl overflow-auto">
-            <div className="flex flex-col w-full h-full">
-              <span className="absolute text-xl font-bold left-6 top-4">
-                Žemaičiai
-              </span>
-              <div className="register-container flex flex-col justify-center py-6 px-44 h-full w-full">
-                <div className="text-container text-center">
-                  <span className="text-3xl text-custom-850 font-extrabold flex justify-center items-center flex-col">
-                    Registracija
-                    <hr className="border-2 mt-4 w-14 border-custom-850 rounded-2xl" />
-                  </span>
-                </div>
-                <ul className="logos-list pt-2 flex justify-center space-x-4 h-max w-full bg-white ">
-                  <li>Logo1</li>
-                  <li>Logo2</li>
-                  <li>Logo3</li>
-                </ul>
-                <span className="text-center pt-6 pb-4 text-xs text-gray-500">
-                  arba sukurkite paskyrą
+            <span className="absolute text-xl font-bold left-6 top-4">
+              Žemaičiai
+            </span>
+            <div className="register-container flex flex-col justify-center py-6 px-44 h-full w-full">
+              <div className="text-container text-center">
+                <span className="text-3xl text-custom-850 font-extrabold flex justify-center items-center flex-col">
+                  Registracija
+                  <hr className="border-2 mt-4 w-14 border-custom-850 rounded-2xl" />
                 </span>
+              </div>
+              <ul className="logos-list pt-2 flex justify-center space-x-4 h-max w-full bg-white ">
+                <li>Logo1</li>
+                <li>Logo2</li>
+                <li>Logo3</li>
+              </ul>
+              <span className="text-center pt-6 pb-4 text-xs text-gray-500">
+                arba sukurkite paskyrą
+              </span>
 
-                <Form method="post">
-                  <div className="space-y-4">
-                    <CustomInput
-                      name="firstname"
-                      type="text"
-                      title="Vardas"
-                      error={actionData?.errors.firstname}
-                    />
-                    <CustomInput
-                      name="lastname"
-                      type="text"
-                      title="Pavardė"
-                      error={actionData?.errors.lastname}
-                    />
-                    <CustomInput
-                      name="username"
-                      type="text"
-                      title="Slapyvardis"
-                      error={actionData?.errors.username}
-                    />
-                    <CustomInput
-                      name="secretCode"
-                      type="text"
-                      title="Pakvietimo kodas"
-                      error={actionData?.errors.secretCode}
-                    />
-                    <CustomInput
-                      name="email"
-                      type="text"
-                      title="El. paštas"
-                      error={actionData?.errors.email}
-                    />
-                    <CustomInput
-                      name="password"
-                      type="password"
-                      title="Slaptažodis"
-                      error={actionData?.errors.password}
-                    />
+              <Form method="post">
+                <div className="space-y-4">
+                  <CustomInput
+                    name="firstname"
+                    type="text"
+                    title="Vardas"
+                    error={actionData?.errors.firstname}
+                  />
+                  <CustomInput
+                    name="lastname"
+                    type="text"
+                    title="Pavardė"
+                    error={actionData?.errors.lastname}
+                  />
+                  <CustomInput
+                    name="username"
+                    type="text"
+                    title="Slapyvardis"
+                    error={actionData?.errors.username}
+                  />
+                  <CustomInput
+                    name="secretCode"
+                    type="text"
+                    title="Pakvietimo kodas"
+                    error={actionData?.errors.secretCode}
+                  />
+                  <CustomInput
+                    name="email"
+                    type="text"
+                    title="El. paštas"
+                    error={actionData?.errors.email}
+                  />
+                  <CustomInput
+                    name="password"
+                    type="password"
+                    title="Slaptažodis"
+                    error={actionData?.errors.password}
+                  />
+                </div>
+                {actionData?.errors?.existingUser ? (
+                  <div
+                    className="font-bold text-red-400 m-0"
+                    id="wrong-credentials-error"
+                  >
+                    {actionData.errors.existingUser}
                   </div>
-                  {actionData?.errors?.existingUser ? (
-                    <div
-                      className="font-bold text-red-400 m-0"
-                      id="wrong-credentials-error"
-                    >
-                      {actionData.errors.existingUser}
-                    </div>
-                  ) : null}
-                  {actionData?.errors?.wrongSecretCode ? (
-                    <div
-                      className="font-bold text-red-400 m-0"
-                      id="wrong-secretCode-error"
-                    >
-                      {actionData.errors.wrongSecretCode}
-                    </div>
-                  ) : null}
-                  <div className="button-container flex flex-col justify-center pt-6 items-center w-full">
-                    <button
-                      type="submit"
-                      className="
+                ) : null}
+                {actionData?.errors?.wrongSecretCode ? (
+                  <div
+                    className="font-bold text-red-400 m-0"
+                    id="wrong-secretCode-error"
+                  >
+                    {actionData.errors.wrongSecretCode}
+                  </div>
+                ) : null}
+                <div className="button-container flex flex-col justify-center pt-6 items-center w-full">
+                  <button
+                    type="submit"
+                    className="
                       rounded-3xl bg-custom-850 px-8 py-3 
                       transition duration-300 ease-in-out hover:bg-custom-800 
                       text-white"
-                    >
-                      Prisiregistruoti
-                    </button>
-                  </div>
-                </Form>
-              </div>
+                  >
+                    Prisiregistruoti
+                  </button>
+                </div>
+              </Form>
             </div>
           </div>
-          <div className="flex relative bg-purple-background bg-cover bg-center justify-center items-center h-full w-1/3 rounded-r-2xl ">
+          <div className="flex relative bg-purple-background bg-cover bg-center justify-center items-center h-full w-1/3 rounded-r-2xl">
             <div className="text-container text-center">
               <span className="text-3xl text-white font-extrabold flex justify-center items-center flex-col">
                 Turi Paskyrą?
--- WebPage/app/routes/login.tsx
+++ WebPage/app/routes/login.tsx
@@ -56,82 +56,80 @@ export default function LoginPage() {
       <div className="flex w-full h-full flex-col justify-center items-center">
         <div className="flex w-2/3 h-full my-32">
           <div className="flex relative h-full w-2/3 bg-custom-200 rounded-l-2xl overflow-auto">
-            <div className="flex flex-col w-full h-full">
-              <span className="absolute text-xl font-bold left-6 top-4">
-                Žemaičiai
-              </span>
-              <div className="login-container flex flex-col justify-center py-6 px-44 h-full w-full">
-                <div className="text-container text-center">
-                  <span className="text-3xl text-custom-850 font-extrabold flex justify-center items-center flex-col">
-                    Prisijungimas
-                    <hr className="border-2 mt-4 w-14 border-custom-850 rounded-2xl" />
-                  </span>
-                </div>
-                <ul className="logos-list pt-2 flex justify-center space-x-4 h-max w-full bg-white ">
-                  <li>Logo1</li>
-                  <li>Logo2</li>
-                  <li>Logo3</li>
-                </ul>
-                <span className="text-center pt-6 pb-4 text-xs text-gray-500">
-                  arba naudokite egzistuojančią paskyrą
+            <span className="absolute text-xl font-bold left-6 top-4">
+              Žemaičiai
+            </span>
+            <div className="login-container flex flex-col justify-center py-6 px-44 h-full w-full">
+              <div className="text-container text-center">
+                <span className="text-3xl text-custom-850 font-extrabold flex justify-center items-center flex-col">
+                  Prisijungimas
+                  <hr className="border-2 mt-4 w-14 border-custom-850 rounded-2xl" />
                 </span>
+              </div>
+              <ul className="logos-list pt-2 flex justify-center space-x-4 h-max w-full bg-white ">
+                <li>Logo1</li>
+                <li>Logo2</li>
+                <li>Logo3</li>
+              </ul>
+              <span className="text-center pt-6 pb-4 text-xs text-gray-500">
+                arba naudokite egzistuojančią paskyrą
+              </span>
 
-                <Form method="post" className="space-y-4">
-                  <CustomInput
-                    name="email"
-                    type="text"
-                    title="El. paštas"
-                    error={actionData?.errors.email}
-                  />
-                  <CustomInput
-                    name="password"
-                    type="password"
-                    title="Slaptažodis"
-                    error={actionData?.errors.password}
-                  />
-                  {actionData?.errors?.wrongCredentials ? (
-                    <div
-                      className="font-bold text-red-400"
-                      id="wrong-credentials-error"
-                    >
-                      {actionData.errors.wrongCredentials}
-                    </div>
-                  ) : null}
-
-                  <input type="hidden" name="redirectTo" value={redirectTo} />
-
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center">
-                      <input
-                        id="remember"
-                        name="remember"
-                        type="checkbox"
-                        className="h-5 w-5 border rounded border-grey-500 accent-custom-850"
-                      />
-                      <label
-                        htmlFor="remember"
-                        className="ml-2 block font-medium"
-                      >
-                        Prisiminti mane
-                      </label>
-                    </div>
+              <Form method="post" className="space-y-4">
+                <CustomInput
+                  name="email"
+                  type="text"
+                  title="El. paštas"
+                  error={actionData?.errors.email}
+                />
+                <CustomInput
+                  name="password"
+                  type="password"
+                  title="Slaptažodis"
+                  error={actionData?.errors.password}
+                />
+                {actionData?.errors?.wrongCredentials ? (
+                  <div
+                    className="font-bold text-red-400"
+                    id="wrong-credentials-error"
+                  >
+                    {actionData.errors.wrongCredentials}
                   </div>
-                  <div className="button-container flex flex-col justify-center pt-8 items-center w-full">
-                    <button
-                      type="submit"
-                      className="
+                ) : null}
+
+                <input type="hidden" name="redirectTo" value={redirectTo} />
+
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center">
+                    <input
+                      id="remember"
+                      name="remember"
+                      type="checkbox"
+                      className="h-5 w-5 border rounded border-grey-500 accent-custom-850"
+                    />
+                    <label
+                      htmlFor="remember"
+                      className="ml-2 block font-medium"
+                    >
+                      Prisiminti mane
+                    </label>
+                  </div>
+                </div>
+                <div className="button-container flex flex-col justify-center pt-8 items-center w-full">
+                  <button
+                    type="submit"
+                    className="
                       w-2/4 rounded-3xl bg-custom-850 px-2 py-3 
                       transition duration-300 ease-in-out hover:bg-custom-800 
                       text-white"
-                    >
-                      Prisijungti
-                    </button>
-                  </div>
-                </Form>
-              </div>
+                  >
+                    Prisijungti
+                  </button>
+                </div>
+              </Form>
             </div>
           </div>
-          <div className="flex relative justify-center items-center bg-custom-800 h-full w-1/3 rounded-r-2xl">
+          <div className="flex relative bg-purple-background bg-cover bg-center justify-center items-center h-full w-1/3 rounded-r-2xl">
             <div className="text-container text-center">
               <span className="text-3xl text-white font-extrabold flex justify-center items-center flex-col">
                 Neturi Paskyros?
