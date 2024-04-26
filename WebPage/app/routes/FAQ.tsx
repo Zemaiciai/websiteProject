91e86e8b3fb0bb512b5e137e1a6334e45ed079e1
nobodiis <nobodyprodbusiness@gmail.com>
2024-04-26 23:51:09 +0300

    Now the created questions in the admin panel is displayed on the faq page. Needs better design

--- WebPage/app/routes/FAQ.tsx
+++ WebPage/app/routes/FAQ.tsx
@@ -1,8 +1,12 @@
+import { FaqPage } from "@prisma/client";
 import { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import { useState } from "react";
+import { useLoaderData } from "@remix-run/react";
+import { json } from "express";
+import { useEffect, useState } from "react";
 import NavBar from "~/components/common/NavBar/NavBar";
 import NavBarHeader from "~/components/common/NavBar/NavBarHeader";
 import NewFooter from "~/components/newFooter/NewFooter";
+import { getFAQQuestions } from "~/models/faqPage.server";
 
 import { requireUser, requireUserId } from "~/session.server";
 export const meta: MetaFunction = () => [{ title: "D.U.K. - Žemaičiai" }];
@@ -10,65 +14,34 @@ export const meta: MetaFunction = () => [{ title: "D.U.K. - Žemaičiai" }];
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const userId = await requireUserId(request);
   const user = await requireUser(request);
-  return null;
+  const faqMessages = await getFAQQuestions();
+  return faqMessages;
 };
 
 const FAQ = () => {
-  const [questions, setQuestions] = useState([
-    {
-      question: "Question1",
-      answer: "Answer1",
-      isOpen: false,
-    },
-    {
-      question: "Question2",
-      answer: "Answer2",
-      isOpen: false,
-    },
-    {
-      question: "Question3",
-      answer: "Answer3",
-      isOpen: false,
-    },
-    {
-      question: "Question4",
-      answer: "Answer4",
-      isOpen: false,
-    },
-    {
-      question: "Question5",
-      answer: "Answer5",
-      isOpen: false,
-    },
-    {
-      question: "Question6",
-      answer: "Answer6",
-      isOpen: false,
-    },
-    {
-      question: "Question7",
-      answer: "Answer7",
-      isOpen: false,
-    },
-    {
-      question: "Question8",
-      answer: "Answer8",
-      isOpen: false,
-    },
-  ]);
-
-  const toggleAnswer = (index: number): void => {
-    setQuestions((prevQuestions) =>
-      prevQuestions.map((q, i) =>
-        i === index ? { ...q, isOpen: !q.isOpen } : q,
-      ),
-    );
-  };
-
+  const faqMessages = useLoaderData<typeof loader>();
   const [activeTab, setActiveTab] = useState("myGroups");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredFAQMessages, setFilteredFAQMessages] = useState<FaqPage[]>([]);
+  const [searchNotFound, setSearchNotFound] = useState(false); // State to track if search is not found
+
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
   };
+
+  // Filter FAQ messages based on search query
+  useEffect(() => {
+    if (!searchQuery) {
+      setFilteredFAQMessages(faqMessages);
+      setSearchNotFound(false); // Reset search not found when search query is empty
+    } else {
+      const filtered = faqMessages.filter((message) =>
+        message.questionName.toLowerCase().includes(searchQuery.toLowerCase()),
+      );
+      setFilteredFAQMessages(filtered);
+      setSearchNotFound(filtered.length === 0); // Set search not found if filtered array is empty
+    }
+  }, [searchQuery, faqMessages]);
 
   return (
     <div className="flex h-screen bg-custom-100">
@@ -83,42 +56,66 @@ const FAQ = () => {
         />
       </div>
 
-      <div className="w-screen h-screen flex flex-grow flex-col bg-custom-100 pb-3">
+      <div className="w-screen h-screen flex flex-col bg-custom-100 pb-3">
         <NavBarHeader title={`${activeTab ? "D.U.K." : "Grupės"}`} />
+        <div className="flex justify-between bg-custom-200 m-3">
+          <div className="mb-4">
+            <ul className="flex flex-wrap -mb-px border-b border-gray-200 pb-3">
+              <div>
+                <h1 className="font-bold text-2xl pt-4 pl-3">
+                  Jei norite lengviau atrasti atsakymą į jūsų klausimą ieškoti
+                  galite čia
+                </h1>
+              </div>
+            </ul>
+            <div className="m-4 w-full">
+              <input
+                type="text"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                placeholder="Search FAQ..."
+                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
+              />
+            </div>
+          </div>
+        </div>
         <div className="flex justify-between bg-custom-200 m-3">
           <div
             className="max-w-full mx-auto mt-5 mb-5 divide-y   shadow shadow-custom-800 rounded-xl"
             style={{ width: "80%" }}
           >
-            {questions.map((item, index) => (
-              <li key={index} className="mb-2 list-none">
-                <details className="group">
-                  <summary className="flex items-center gap-3 px-4 py-1 font-medium marker:content-none hover:cursor-pointer">
-                    <svg
-                      className="w-5 h-5 text-gray-500 transition group-open:rotate-90"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
-                      ></path>
-                    </svg>
-                    <span>{item.question}</span>
-                  </summary>
-                  <article className="px-4 pb-4">
-                    <p>{item.answer}</p>
-                  </article>
-                </details>
-              </li>
-            ))}
+            {searchNotFound ? (
+              <div className="m-3 font-bold text-black">
+                Dėja, bet nieko neradome.
+              </div>
+            ) : (
+              filteredFAQMessages.map((item, index) => (
+                <li key={index} className="m-3 list-none">
+                  <details className="group ">
+                    <summary className="flex items-center gap-3 px-4 py-1 font-medium marker:content-none hover:cursor-pointer mt-5 pb-3">
+                      <svg
+                        className="w-5 h-5 text-gray-500 transition group-open:rotate-90"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                        ></path>
+                      </svg>
+                      <span className="">{item.questionName}</span>
+                    </summary>
+                    <article className="px-4 pb-4">
+                      <p>{item.questionAnswer}</p>
+                    </article>
+                  </details>
+                </li>
+              ))
+            )}
           </div>
-        </div>
-        <div className="mt-3">
-          <NewFooter />
         </div>
       </div>
     </div>
