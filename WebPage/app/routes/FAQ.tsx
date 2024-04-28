6bb9363684e8cd8233043f2cd4fe6ffda956106b
nobodiis <nobodyprodbusiness@gmail.com>
2024-04-28 15:30:40 +0300

    Fixed design

--- WebPage/app/routes/FAQ.tsx
+++ WebPage/app/routes/FAQ.tsx
@@ -1,14 +1,13 @@
 import { FaqPage } from "@prisma/client";
 import { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { json } from "express";
 import { useEffect, useState } from "react";
 import NavBar from "~/components/common/NavBar/NavBar";
 import NavBarHeader from "~/components/common/NavBar/NavBarHeader";
 import NewFooter from "~/components/newFooter/NewFooter";
 import { getFAQQuestions } from "~/models/faqPage.server";
-
 import { requireUser, requireUserId } from "~/session.server";
+
 export const meta: MetaFunction = () => [{ title: "D.U.K. - Žemaičiai" }];
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -23,23 +22,22 @@ const FAQ = () => {
   const [activeTab, setActiveTab] = useState("myGroups");
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredFAQMessages, setFilteredFAQMessages] = useState<FaqPage[]>([]);
-  const [searchNotFound, setSearchNotFound] = useState(false); // State to track if search is not found
+  const [searchNotFound, setSearchNotFound] = useState(false);
 
   const handleTabClick = (tab: string) => {
     setActiveTab(tab);
   };
 
-  // Filter FAQ messages based on search query
   useEffect(() => {
     if (!searchQuery) {
       setFilteredFAQMessages(faqMessages);
-      setSearchNotFound(false); // Reset search not found when search query is empty
+      setSearchNotFound(false);
     } else {
       const filtered = faqMessages.filter((message) =>
         message.questionName.toLowerCase().includes(searchQuery.toLowerCase()),
       );
       setFilteredFAQMessages(filtered);
-      setSearchNotFound(filtered.length === 0); // Set search not found if filtered array is empty
+      setSearchNotFound(filtered.length === 0);
     }
   }, [searchQuery, faqMessages]);
 
@@ -59,31 +57,29 @@ const FAQ = () => {
       <div className="w-screen h-screen flex flex-col bg-custom-100 pb-3">
         <NavBarHeader title={`${activeTab ? "D.U.K." : "Grupės"}`} />
         <div className="flex justify-between bg-custom-200 m-3">
-          <div className="mb-4">
-            <ul className="flex flex-wrap -mb-px border-b border-gray-200 pb-3">
-              <div>
-                <h1 className="font-bold text-2xl pt-4 pl-3">
-                  Jei norite lengviau atrasti atsakymą į jūsų klausimą ieškoti
-                  galite čia
-                </h1>
-              </div>
-            </ul>
+          <div className="mb-1">
+            <div className="w-full px-5">
+              {" "}
+              {/* Adjusted width and added padding */}
+              <h1 className="font-bold text-2xl pt-4">
+                Jei norite lengviau atrasti atsakymą į jūsų klausimą ieškoti
+                galite čia
+              </h1>
+            </div>
+
             <div className="m-4 w-full">
               <input
                 type="text"
                 value={searchQuery}
                 onChange={(e) => setSearchQuery(e.target.value)}
-                placeholder="Search FAQ..."
+                placeholder="Ieškoti klausimo..."
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
               />
             </div>
           </div>
         </div>
-        <div className="flex justify-between bg-custom-200 m-3">
-          <div
-            className="max-w-full mx-auto mt-5 mb-5 divide-y   shadow shadow-custom-800 rounded-xl"
-            style={{ width: "80%" }}
-          >
+        <div className="flex bg-custom-200 ml-3 mr-3 mb-3">
+          <div className="w-full mx-auto mt-5 mb-5 ml-5 mr-5 divide-y shadow shadow-custom-800 rounded-xl">
             {searchNotFound ? (
               <div className="m-3 font-bold text-black">
                 Dėja, bet nieko neradome.
