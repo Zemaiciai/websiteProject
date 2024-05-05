13dc27cd4a0dc06ecb7704ef30e38e0149141f6e
Gytis Pranauskas <gpgytis@gmail.com>
2024-05-05 16:40:55 +0300

    shortened Notifications.tsx code

--- WebPage/app/components/common/NavBar/Notifications.tsx
+++ WebPage/app/components/common/NavBar/Notifications.tsx
@@ -20,15 +20,10 @@ export default function Notifications() {
   }, [hideSeen]);
 
   const location = useLocation();
-  const handleFormSubmission = (form: HTMLFormElement | null) => {
-    if (form) {
-      form.submit();
-    }
-  };
 
-  const handleHideSeen = () => {
-    setHideSeen(!hideSeen);
-  };
+  const handleFormSubmission = (form: HTMLFormElement | null) =>
+    form && form.submit();
+  const handleHideSeen = () => setHideSeen(!hideSeen);
 
   const seenNotifications = allNotifications?.filter(
     (n: Notification) => n.isSeen === true,
@@ -42,131 +37,113 @@ export default function Notifications() {
    * assigned, accpeted, declined, changed status, completed, payed
    */
   const getNotificationLocation = (
-    notificationType: NotificationTypes,
-    notification: Notification,
+    type: NotificationTypes,
+    n: Notification,
   ) => {
-    let redirectTo = "";
-
-    switch (notificationType) {
-      case NotificationTypes.FRIEND_REQUEST:
-      case NotificationTypes.FRIEND_DECLINED:
-      case NotificationTypes.FRIEND_ACCEPTED:
-      case NotificationTypes.FRIEND_REMOVE:
-        redirectTo = `/profile/${notification.senderId}`;
-        break;
-      case NotificationTypes.ORDER_ACCEPTED:
-      case NotificationTypes.ORDER_ASSIGNED:
-      case NotificationTypes.ORDER_COMPLETED:
-      case NotificationTypes.ORDER_DECLINED:
-      case NotificationTypes.ORDER_IN_PROGRESS:
-      case NotificationTypes.ORDER_PAYED:
-        redirectTo = "/orders";
-        break;
-      default:
-        redirectTo = "404";
-        break;
-    }
-
-    return redirectTo;
+    const redirectTo = {
+      FRIEND_REQUEST: `/profile/${n.senderId}`,
+      FRIEND_DECLINED: `/profile/${n.senderId}`,
+      FRIEND_ACCEPTED: `/profile/${n.senderId}`,
+      FRIEND_REMOVE: `/profile/${n.senderId}`,
+      ORDER_ACCEPTED: "/orders",
+      ORDER_ASSIGNED: "/orders",
+      ORDER_COMPLETED: "/orders",
+      ORDER_DECLINED: "/orders",
+      ORDER_IN_PROGRESS: "/orders",
+      ORDER_PAYED: "/orders",
+    };
+    return redirectTo[type] ?? "404";
   };
 
-  return (
-    <div
-      className="
-      notifications flex flex-col
-      absolute h-48 w-[33.75rem] right-0 top-7 
-      bg-custom-200 drop-shadow-lg rounded p-4 overflow-auto"
-    >
-      <span className="text-2xl">Pranešimai</span>
+  const renderNotifications = (notifications: Notification[]) =>
+    notifications?.map((n, index) => (
+      <li
+        key={n.id ?? index}
+        className="group flex px-1 py-2 w-full justify-between items-center first:pt-2 hover:bg-gray-200"
+      >
+        <Form
+          method="post"
+          className="flex w-[400px] cursor-pointer"
+          action="/notifications"
+          onClick={(e) => handleFormSubmission(e.currentTarget)}
+        >
+          <input
+            id={`notification-${index}`}
+            name="notificationId"
+            type="hidden"
+            value={n.id}
+            hidden
+            readOnly
+          />
+          <input
+            id={`location-${index}`}
+            name="location"
+            type="hidden"
+            value={getNotificationLocation(n.notificationType, n)}
+            hidden
+            readOnly
+          />
+          <input
+            id={`special-${index}`}
+            name="clickedOnNotification"
+            type="hidden"
+            value="true"
+            hidden
+            readOnly
+          />
+          <div className={`${n.isSeen && "opacity-50"} hover:opacity-100`}>
+            {n.message}
+            <span className="text-[#626f86] font-normal text-nowrap text-sm pl-1">
+              {useHowLongAgo(new Date(n.createdAt))}
+            </span>
+          </div>
+        </Form>
+        <div className="flex items-center">
+          <Form method="post" action="/notifications">
+            <input
+              id={`notification-id-${index}`}
+              name="notificationId"
+              type="changeReadStatus"
+              value={n.id}
+              hidden
+              readOnly
+            />
+            <input
+              id={`location-${index}`}
+              name="location"
+              type="changeReadStatus"
+              value={location.pathname}
+              hidden
+              readOnly
+            />
+            <button
+              className={`h-5 w-5 opacity-0 flex items-center justify-center border rounded-full border-custom-800 accent-custom-850 cursor-pointer group-hover:opacity-100 ${
+                n.isSeen && "bg-custom-800"
+              }`}
+              type="submit"
+            ></button>
+          </Form>
+        </div>
+      </li>
+    ));
 
+  return (
+    <div className="notifications flex flex-col absolute h-48 w-[33.75rem] right-0 top-7 bg-custom-200 drop-shadow-lg rounded p-4 overflow-auto">
+      <span className="text-2xl">Pranešimai</span>
       <div className="flex items-center justify-center mt-4 mb-2">
         <span className="text-nowrap mr-2 text-sm">Nauji pranešimai</span>
         <hr className="flex justify-center border-2 w-full border-custom-850 rounded-2xl" />
       </div>
-
       {!notifications || notifications.length <= 0 ? (
         <span className="text-sm font-normal">Nėra naujų pranešimų!</span>
       ) : (
         <div>
           <ul className="flex flex-col overflow-auto">
-            {notifications.map((n, index) => (
-              <li
-                key={n.id ?? index}
-                className="group flex px-1 py-2 w-full justify-between items-center first:pt-2 hover:bg-gray-200"
-              >
-                <Form
-                  method="post"
-                  className="flex w-[400px] cursor-pointer"
-                  action="/notifications"
-                  onClick={(e) =>
-                    handleFormSubmission(e.currentTarget as HTMLFormElement)
-                  }
-                >
-                  <input
-                    id={`notification-${index}`}
-                    name="notificationId"
-                    type="hidden"
-                    value={n.id}
-                    hidden
-                    readOnly
-                  />
-                  <input
-                    id={`location-${index}`}
-                    name="location"
-                    type="hidden"
-                    value={getNotificationLocation(n.notificationType, n)}
-                    hidden
-                    readOnly
-                  />
-                  <input
-                    id={`special-${index}`}
-                    name="clickedOnNotification"
-                    type="hidden"
-                    value={"true"}
-                    hidden
-                    readOnly
-                  />
-                  <div
-                    className={`${n.isSeen && "opacity-50"} hover:opacity-100`}
-                  >
-                    {n.message}
-                    <span className="text-[#626f86] font-normal text-nowrap text-sm pl-1">
-                      {useHowLongAgo(new Date(n.createdAt))}
-                    </span>
-                  </div>
-                </Form>
-                <div className="flex items-center">
-                  <Form method="post" name="form" action="/notifications">
-                    <input
-                      id={`notification-id-${index}`}
-                      name="notificationId"
-                      type="changeReadStatus"
-                      value={n.id}
-                      hidden
-                      readOnly
-                    />
-                    <input
-                      id={`location-${index}`}
-                      name="location"
-                      type="changeReadStatus"
-                      value={location.pathname}
-                      hidden
-                      readOnly
-                    />
-                    <button
-                      className={`h-5 w-5 opacity-0 flex items-center justify-center border rounded-full border-custom-800 accent-custom-850 cursor-pointer
-                  group-hover:opacity-100 ${n.isSeen && "bg-custom-800"}`}
-                      type="submit"
-                    ></button>
-                  </Form>
-                </div>
-              </li>
-            ))}
+            {renderNotifications(notifications)}
           </ul>
         </div>
       )}
-
-      {!seenNotifications || seenNotifications.length <= 0 ? null : (
+      {allNotifications && allNotifications.some((n) => n.isSeen) && (
         <>
           <div
             className="flex items-center justify-center mt-4 mb-2 cursor-pointer"
@@ -192,79 +169,7 @@ export default function Notifications() {
                 !hideSeen && "scale-y-0"
               }`}
             ></div>
-            {seenNotifications.map((n, index) => (
-              <li
-                key={n.id ?? index}
-                className="group flex px-1 py-2 w-full justify-between items-center first:pt-2 hover:bg-gray-200"
-              >
-                <Form
-                  method="post"
-                  className="flex w-[400px] cursor-pointer"
-                  action="/notifications"
-                  onClick={(e) =>
-                    handleFormSubmission(e.currentTarget as HTMLFormElement)
-                  }
-                >
-                  <input
-                    id={`notification-${index}`}
-                    name="notificationId"
-                    type="hidden"
-                    value={n.id}
-                    hidden
-                    readOnly
-                  />
-                  <input
-                    id={`location-${index}`}
-                    name="location"
-                    type="hidden"
-                    value={getNotificationLocation(n.notificationType, n)}
-                    hidden
-                    readOnly
-                  />
-                  <input
-                    id={`special-${index}`}
-                    name="clickedOnNotification"
-                    type="hidden"
-                    value={"true"}
-                    hidden
-                    readOnly
-                  />
-                  <div
-                    className={`${n.isSeen && "opacity-50"} hover:opacity-100`}
-                  >
-                    {n.message}
-                    <span className="text-[#626f86] font-normal text-nowrap text-sm pl-1">
-                      {useHowLongAgo(new Date(n.createdAt))}
-                    </span>
-                  </div>
-                </Form>
-                <div className="relative">
-                  <Form method="post" name="form" action="/notifications">
-                    <input
-                      id={`notification-id-${index}`}
-                      name="notificationId"
-                      type="changeReadStatus"
-                      value={n.id}
-                      hidden
-                      readOnly
-                    />
-                    <input
-                      id={`location-${index}`}
-                      name="location"
-                      type="changeReadStatus"
-                      value={location.pathname}
-                      hidden
-                      readOnly
-                    />
-                    <button
-                      className={`h-5 w-5 opacity-0 flex justify-center border rounded-full border-custom-800 accent-custom-850
-                      group-hover:opacity-100 ${n.isSeen && "bg-custom-800"}`}
-                      type="submit"
-                    />
-                  </Form>
-                </div>
-              </li>
-            ))}
+            {renderNotifications(allNotifications.filter((n) => n.isSeen))}
           </ul>
         </>
       )}
