bd8faffc48e3c771fa6fd4c69c6850a58465b188
Gytis Pranauskas <gpgytis@gmail.com>
2024-05-06 20:27:12 +0300

    removed imports

--- WebPage/app/routes/messages.tsx
+++ WebPage/app/routes/messages.tsx
@@ -1,15 +1,7 @@
 import { MetaFunction, Outlet } from "@remix-run/react";
-import { useState } from "react";
-import NavBar from "~/components/common/NavBar/NavBar";
-import NavBarHeader from "~/components/common/NavBar/NavBarHeader";
 export const meta: MetaFunction = () => [{ title: "Žemaičiai" }];
 
 const Dashboard = () => {
-  const [activeTab, setActiveTab] = useState("myGroups");
-  const handleTabClick = (tab: string) => {
-    setActiveTab(tab);
-  };
-
   return (
     <div className="flex h-full">
       <Outlet />
