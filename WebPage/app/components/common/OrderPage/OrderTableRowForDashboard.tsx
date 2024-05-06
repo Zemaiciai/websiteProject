a88f704397e5988bf0059eff8fdfd4e819ab14eb
nobodiis <nobodyprodbusiness@gmail.com>
2024-05-06 22:14:00 +0300

    Fixed a bug

--- WebPage/app/components/common/OrderPage/OrderTableRowForDashboard.tsx
+++ WebPage/app/components/common/OrderPage/OrderTableRowForDashboard.tsx
@@ -40,11 +40,12 @@ export default function OrderCard({ createdBy, order }: OrderCardProps) {
           <td className="order-status text-center truncate">
             {order.orderStatus}
           </td>
-          {order.orderStatus !== OrderStatus.PAYED && (
-            <td className="order-status text-center truncate">
-              <OrderTimer orderEndDate={order.completionDate}></OrderTimer>
-            </td>
-          )}
+          {order.orderStatus !== OrderStatus.PAYED &&
+            order.orderStatus !== OrderStatus.COMPLETED && (
+              <td className="order-status text-center truncate">
+                <OrderTimer orderEndDate={order.completionDate}></OrderTimer>
+              </td>
+            )}
         </>
       ) : (
         <>
@@ -64,11 +65,12 @@ export default function OrderCard({ createdBy, order }: OrderCardProps) {
           >
             {order.orderStatus}
           </td>
-          {order.orderStatus !== OrderStatus.PAYED && (
-            <td className="order-status text-center truncate">
-              <OrderTimer orderEndDate={order.completionDate}></OrderTimer>
-            </td>
-          )}
+          {order.orderStatus !== OrderStatus.PAYED &&
+            order.orderStatus !== OrderStatus.COMPLETED && (
+              <td className="order-status text-center truncate">
+                <OrderTimer orderEndDate={order.completionDate}></OrderTimer>
+              </td>
+            )}
         </>
       )}
     </tr>
