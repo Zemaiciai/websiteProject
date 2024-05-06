5d0b9fe768048e7bedbf8caf1ca4cbb366925ac0
nobodiis <nobodyprodbusiness@gmail.com>
2024-05-06 17:00:48 +0300

    Made so that in the dashboard you can see the 5 recent orders

---
+++ WebPage/app/components/common/OrderPage/OrderTableRowForDashboard.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import OrderTimer from "./OrderTimer";
+import { User } from "~/models/user.server";
+import { Form } from "@remix-run/react";
+import { Order, OrderStatus } from "@prisma/client";
+import { useUser } from "~/utils";
+interface OrderCardProps {
+  order: Order;
+  createdBy: User["userName"];
+}
+
+export default function OrderCard({ createdBy, order }: OrderCardProps) {
+  const user = useUser();
+
+  const [ended, setEnded] = useState(false);
+  const [canPay, setCanPay] = useState(false);
+
+  useEffect(() => {
+    if (
+      (ended &&
+        order.orderStatus !== OrderStatus.PAYED &&
+        (order.orderStatus === OrderStatus.ACCEPTED ||
+          order.orderStatus === OrderStatus.COMPLETED)) ||
+      order.orderStatus === OrderStatus.COMPLETED
+    ) {
+      setCanPay(true);
+    }
+  }, [order.orderStatus]);
+
+  return (
+    <tr className="order-card-row bg-white outline outline-1 outline-gray-100">
+      {user.role === "Darbuotojas" ? (
+        <>
+          <td className="order-name text-center max-w-[50px] truncate">
+            {createdBy}
+          </td>
+          <td className="order-name text-center max-w-[100px] truncate">
+            {order.orderName}
+          </td>
+          <td className="order-status text-center truncate">
+            {order.orderStatus}
+          </td>
+        </>
+      ) : (
+        <>
+          <td className="order-name text-center max-w-[100px] truncate">
+            {createdBy}
+          </td>
+          <td className="order-name text-center max-w-[100px] truncate">
+            {order.orderName}
+          </td>
+          <td
+            className={`${
+              order.orderStatus === OrderStatus.ACCEPTED && "text-lime-500"
+            } 
+            ${
+              order.orderStatus === OrderStatus.DECLINED && "text-red-500"
+            } order-name text-center max-w-[100px] truncate`}
+          >
+            {order.orderStatus}
+          </td>
+        </>
+      )}
+    </tr>
+  );
+}
--- WebPage/app/routes/Dashboard.tsx
+++ WebPage/app/routes/Dashboard.tsx
@@ -2,7 +2,7 @@ import { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
 import { useState } from "react";
 import { typedjson, useTypedLoaderData } from "remix-typedjson";
 import Message from "~/components/DashBoardCustomMessagesDesign/Message";
-import OrdersTable from "~/components/common/OrderPage/OrdersTable";
+import OrdersTableForDashboard from "~/components/common/OrderPage/OrdersTableForDashboard";
 
 import { getAllMessages } from "~/models/customMessage.server";
 import { getOrdersByUserId } from "~/models/order.server";
@@ -25,21 +25,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 const Dashboard = () => {
   const data = useTypedLoaderData<typeof loader>();
   const [worker, setWorker] = useState(false);
-  const [searchQueries, setSearchQueries] = useState<{ [key: string]: string }>(
-    {
-      mainTable: "",
-      importantTable: "",
-    },
-  );
-  const handleSearch = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    tableName: string,
-  ) => {
-    setSearchQueries({
-      ...searchQueries,
-      [tableName]: event.target.value,
-    });
-  };
+
   return (
     <div className="flex h-full w-full">
       <div className="flex flex-col h-full w-full bg-custom-100 overflow-auto">
@@ -51,16 +37,14 @@ const Dashboard = () => {
               </div>
             ),
         )}
-        <div className="flex grow mb-4 ml-4 mr-4">
-          <div className="pt-2 pl-6 pr-6 pb-6 bg-custom-200 text-medium mt-3 ml-3 mr-1 w-full md:w-[calc(100% - 360px)]">
-            <h1 className="text-3xl font-mono font-font-extralight pb-3">
+        <div className="flex  mb-4 ml-4 mr-4">
+          <div className="pt-2 pl-6 pr-6 pb-6 bg-custom-200 text-medium mt-3 ml-3 mr-1 w-full h-full ">
+            <h1 className="text-3xl font-mono font-font-extralight pb-3 pt-2">
               Penki naujausi užsakymai
             </h1>
             <div className="w-full">
-              <OrdersTable
+              <OrdersTableForDashboard
                 orderCards={data.orders}
-                handleSearch={(event) => handleSearch(event, "mainTable")}
-                searchQuery={searchQueries.mainTable}
                 title={`${worker ? "Darbų sąrašas" : "Jūsų užsakymų sąrašas"}`}
               />
             </div>
