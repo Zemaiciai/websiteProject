fe5a6720af5346a0ad04de093084b784c73a0ffd
Gytis Pranauskas <gpgytis@gmail.com>
2024-05-20 20:08:07 +0300

    added @vitest/v8-coverage to get our unit test coverage

--- WebPage/vitest.config.unit.ts
+++ WebPage/vitest.config.unit.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["app/unitTests/*.test.ts"],
+    coverage: {
+      provider: "v8",
+    },
   },
   resolve: {
     alias: {
